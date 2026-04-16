13be9ca7164e93e9e12c3b04480073d77686e36a
yeji <chlrhhan@gmail.com>
2026-04-16 14:49:04 +0900

    def study

---
+++ 11_def.py
@@ -0,0 +1,17 @@
+def hello():
+    print('hi')
+hello()
+
+def hello1(greeting,*names):
+    for name in names:
+        print(greeting,name)
+
+hello1("hi","me","you","are","ho","ha")
+
+# 함수 호출 시 매개변수 명 사용 
+def function(first,second,third):
+    return first + second + third
+
+print(function(3,5,7))
+print(function(first=3,second=5,third=7))
+print(function(second=5,third=7,first=3))
