2ea026d45d984dd2862699b1a74dd50b7b724d42
yeji <chlrhhan@gmail.com>
2026-04-16 14:04:17 +0900

    all 99dan

--- 07_quiz_99dan.py
+++ 07_quiz_99dan.py
@@ -3,3 +3,9 @@ dan = int(dan_str)
 
 for i in range(1,10):
     print('{} X {} = {}'.format(dan,i,dan * i))
+
+# 전체 구구단 출력 
+for i in range(1,10):
+    print('---{}단---'.format(i))
+    for j in range(1,10):
+        print('{} X {} = {}'.format(i,j,i*j))
