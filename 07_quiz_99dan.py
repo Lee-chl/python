21f7812cefcaa8a5d4fc5c49ccf017fba9485cfb
yeji <chlrhhan@gmail.com>
2026-04-16 12:46:40 +0900

    99dan

--- 07_quiz_99dan.py
+++ 07_quiz_99dan.py
@@ -2,4 +2,4 @@ dan_str = input('몇단을 출력할까요?')
 dan = int(dan_str)
 
 for i in range(1,10):
-    print('{} X {} = {}'.format(str(dan),str(i),str(dan * i)))
+    print('{} X {} = {}'.format(dan,i,dan * i))
