c99ce6ff2773c84688a5e1971ad4cabbe978c61f
yeji <chlrhhan@gmail.com>
2026-04-16 11:15:09 +0900

    operator study

---
+++ 01_operator.py
@@ -0,0 +1,14 @@
+print(5**3)
+print(20%3)
+print(4*2)
+print(1/2)
+print(5//3)  #나눗셈 몫 
+print(type(1.1))
+print(type('Hello'))
+num = 3
+num *=3;
+print(num)
+print('Hello' > 'World!')
+print( 5 < 6)
+result = 13 ==4
+print(result)
